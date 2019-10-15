ae656dcc6e56d444c86f6229ffc0222cccca7565
mpoehler <marco@kontaktlinsen-preisvergleich.de>
2019-10-15 10:50:28 +0200

    updated ProductModel

--- app/src/models/ProductModel.ts
+++ app/src/models/ProductModel.ts
@@ -2,9 +2,12 @@ export default class ProductModel {
   name: string = '';
   size: string = '';
 
-  populateFromQuery(query: object): void {
-    Object.keys(query).forEach(key => {
-      
-    });
+  populateFromQuery(query: any): void {
+    if (Object.keys(query).includes("name")) {
+      this.name = query.name;
+    }
+    if (Object.keys(query).includes("size")) {
+      this.size = query.size;
+    }
   }
 }
