6c5b922719d3e19cf1b00a15ffe5bf53c82751e9
mpoehler <marco@kontaktlinsen-preisvergleich.de>
2019-10-15 23:16:41 +0200

    moved to inhected manifest serviceWorker

--- createSW.js
+++ createSW.js
@@ -1,44 +1,18 @@
-// Inside of build.js:
-const {generateSW} = require('workbox-build');
+const workboxBuild = require('workbox-build');
 
-const swDest = './public/sw.js';
-generateSW({
-  swDest,
-  globDirectory: './public',
-  globPatterns: ['app/**/*.{js,png,html,css,svg}'],    // precache app content
-  navigateFallback: '/app/index.html',
-  importWorkboxFrom: 'local',
-  cacheId: 'pre-cache',
-  directoryIndex: 'index.html',
-  maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
-  runtimeCaching: [{
-    urlPattern: new RegExp('/^https:\/\/fonts\.googleapis\.com/'),
-    handler: 'StaleWhileRevalidate',
-    options: {
-        cacheName: 'google-fonts-stylesheets'
-    }
-  },{
-    urlPattern: new RegExp('/^https:\/\/fonts\.gstatic\.com/#'),
-    handler: 'CacheFirst',
-    options: {
-        cacheName: 'google-fonts-webfonts',
-        cacheableResponse: {
-            statuses: [0, 200]
-        },
-        expiration: {
-            maxEntries: 30,
-            maxAgeSeconds: 60 * 60 * 24 * 365
-        }
-    }
-  },{
-    urlPattern: new RegExp('/^https:\/\/cdn\.ampproject\.org/'),
-    handler: 'StaleWhileRevalidate',
-    options: {
-        cacheName: 'amp-cache'
-    }
-  }
-],
-  // Other configuration options...
-}).then(({count, size}) => {
-  console.log(`Generated ${swDest}, which will precache ${count} files, totaling ${size} bytes.`);
-});
+// NOTE: This should be run *AFTER* all your assets are built
+const buildSW = () => {
+  // This will return a Promise
+  return workboxBuild.injectManifest({
+    swSrc: 'sw.js',
+    swDest: 'public/sw.js',
+    globDirectory: './public',
+    globPatterns: ['app/**/*.{js,png,html,css,svg}'],
+    maximumFileSizeToCacheInBytes: 20 *1024 * 1024
+  }).then(({count, size, warnings}) => {
+    // Optionally, log any warnings and details.
+    warnings.forEach(console.warn);
+    console.log(`${count} files will be precached, totaling ${size} bytes.`);
+  });
+}
+buildSW();
