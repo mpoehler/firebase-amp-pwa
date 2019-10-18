621afafb739dfd9ba6fd98a1ba39ec90d7c945b9
mpoehler <marco@kontaktlinsen-preisvergleich.de>
2019-10-18 15:50:26 +0200

    added password authentication, reset password, password-links, Google Signin

--- app/src/main.ts
+++ app/src/main.ts
@@ -31,7 +31,8 @@ firebase.initializeApp(firebaseConfig);
 
 let app: Vue | null = null;
 
-firebase.auth().onAuthStateChanged(() => {
+firebase.auth().onAuthStateChanged(user => {
+  // initialize App when firebase auth is ready
   if (!app) {
     app = new Vue({
       router,
@@ -39,5 +40,27 @@ firebase.auth().onAuthStateChanged(() => {
       vuetify,
       render: h => h(App)
     }).$mount("#app");
+  }
+
+  // set a __session cookie with an IdToken, to enable cloud functions to identify the userbundleRenderer.renderToStream
+  if (user) {
+    user.getIdToken(true).then(token => {
+      const date = new Date();
+      // Set it expire in 7 days
+      date.setTime(date.getTime() + 7 * 24 * 60 * 60 * 1000);
+      document.cookie = `__session=${token}; expires=${date.toUTCString()}; path=/`;
+    });
+  }
+
+  // send verification email to unverified user after login
+  if (user && !user.emailVerified) {
+    user
+      .sendEmailVerification({
+        url: window.location.href
+      })
+      .catch(error => {
+        // problem occured when sending the email
+        console.error("Error on sending email: " + error.message);
+      });
   }
 });
--- app/src/views/SignIn.vue
+++ app/src/views/SignIn.vue
@@ -5,12 +5,61 @@
         v-card-text
           .login
             h3 Sign In
-            v-text-field(ref="email",v-model="email",label="Email",placeholder="your@email.com",outlined,autocomplete="email",name="email")
-            v-text-field(ref="password",v-model="password",:append-icon="showPassword ? mdiEye : mdiEyeOff",:rules="[rules.required, rules.min]",:type="showPassword ? 'text' : 'password'",name="password",label="Password",counter,@click:append="showPassword = !showPassword",outlined)
+            v-text-field(ref="email",v-model="email",label="Email",placeholder="your@email.com",outlined,autocomplete="email",name="email",:error-messages="emailError")
+            v-text-field(ref="password",v-model="password",:append-icon="showPassword ? mdiEye : mdiEyeOff",:rules="[rules.required, rules.min]",:type="showPassword ? 'text' : 'password'",name="password",label="Password",counter,@click:append="showPassword = !showPassword",outlined,:error-messages="passwordError",@change="login")
+            v-btn(ref="passwordReset",small,text,@click="resetPassword") Reset Password
+            v-btn(ref="sendPasswordLink",small,text,@click="sendPasswordLink") Send Password Link
+            v-btn(ref="signInWithGoogle",small,text,@click="signInWithGoogle") SignIn with Google
+  
         v-card-actions
            v-btn(text,to="/signup") No Account? SignUp!
            div.flex-grow-1
            v-btn(large,color="primary",@click="login") Login
+
+    v-dialog(v-model="accountLockedDialog",max-width="290")
+      v-card
+        v-card-title(class="headline") Account locked
+        v-card-text Your account has been locked. Please contact us. 
+        v-card-actions
+          v-spacer
+          v-btn(color="green darken-1",text,@click="accountLockedDialog = false") Close
+    v-dialog(v-model="userUnknownDialog",max-width="290")
+      v-card
+        v-card-title(class="headline") User unknown
+        v-card-text A user with this email could not be found. Would you like to create one?
+        v-card-actions
+          v-spacer
+          v-btn(color="green darken-1",text,@click="userUnknownDialog = false") Close
+          div.flex-grow-1
+          v-btn(color="green darken-1",text,to="/signup") Create
+    v-dialog(v-model="signInLinkDialog",max-width="290")
+      v-card
+        v-card-title(class="headline") Magic Link sent!
+        v-card-text The magic link has been sent to the email address you entered - Just click on the link and you are logged in.
+        v-card-actions
+          v-spacer
+          v-btn(color="green darken-1",text,@click="signInLinkDialog = false") Close
+    v-dialog(v-model="signInLinkExpiredDialog",max-width="290")
+      v-card
+        v-card-title(class="headline") Magic Link expired!
+        v-card-text The magic link has expired, please try again.
+        v-card-actions
+          v-spacer
+          v-btn(color="green darken-1",text,@click="signInLinkExpiredDialog = false") Close
+    v-dialog(v-model="socialSignInProblemDialog",max-width="290")
+      v-card
+        v-card-title(class="headline") Login failed / Popup failed
+        v-card-text Please make sure that this page can open a popup!
+        v-card-actions
+          v-spacer
+          v-btn(color="green darken-1",text,@click="socialSignInProblemDialog = false") Close
+    v-dialog(v-model="signInMethodsDialog",max-width="290")
+      v-card
+        v-card-title(class="headline") Another sign in method exists for your eMail
+        v-card-text {{signinMethodText}}
+        v-card-actions
+          v-spacer
+          v-btn(color="green darken-1",text,@click="signInMethodsDialog = false") Close
 </template>
 
 <style lang="scss" scoped>
@@ -33,8 +82,17 @@ export default class SignIn extends Vue {
   mdiEye = mdiEye;
   mdiEyeOff = mdiEyeOff;
   password: string = "";
+  passwordError: string = "";
   email: string = "";
+  emailError: string = "";
   showPassword = false;
+  accountLockedDialog = false;
+  userUnknownDialog = false;
+  signInLinkDialog = false;
+  signInLinkExpiredDialog = false;
+  socialSignInProblemDialog = false;
+  signInMethodsDialog = false;
+  signinMethodText = "";
   rules = {
     required: (value: String) => !!value || "Required.",
     min: (v: String) => {
@@ -49,24 +107,137 @@ export default class SignIn extends Vue {
       user = await firebase
         .auth()
         .signInWithEmailAndPassword(this.email, this.password);
-      if (user && user.user) {
-        let token = await user.user.getIdToken(true);
-
-        const date = new Date();
-        // Set it expire in 7 days
-        date.setTime(date.getTime() + 7 * 24 * 60 * 60 * 1000);
-        document.cookie = `__session=${token}; expires=${date.toUTCString()}; path=/`;
-
-        this.$router.replace("/");
-      }
+      this.$router.replace("/");
     } catch (error) {
-      // Here an error should be handled.
-      // console.error("Oops. " + error.message);
+      switch (error.code) {
+        case "auth/user-disabled":
+          this.accountLockedDialog = true;
+          break;
+        case "auth/user-not-found":
+          this.userUnknownDialog = true;
+          this.$store.dispatch("setEMail", this.email);
+          break;
+        case "auth/invalid-email":
+          this.emailError = "Email format invalid";
+          break;
+        case "auth/wrong-password":
+          this.passwordError = "Wrong password";
+          break;
+        default:
+          console.error("Unknown error code: " + error.code);
+      }
     }
+  }
+
+  resetPassword() {
+    firebase
+      .auth()
+      .sendPasswordResetEmail(this.email, {
+        url: window.location.href
+      })
+      .catch(error => {
+        switch (error.code) {
+          case "auth/invalid-email":
+            this.emailError = "Please provide an email first";
+            break;
+          case "auth/user-not-found":
+            this.userUnknownDialog = true;
+            break;
+          default:
+            console.error(
+              `Unexpected error ${error.code} occured: ${error.message}`
+            );
+        }
+      });
+  }
+
+  sendPasswordLink() {
+    firebase
+      .auth()
+      .sendSignInLinkToEmail(this.email, {
+        url: `${window.location.href}/?email=${this.email}`,
+        handleCodeInApp: true
+      })
+      .then(() => {
+        this.signInLinkDialog = true;
+      })
+      .catch(error => {
+        switch (error.code) {
+          case "auth/invalid-email":
+            this.emailError = "Please provide a valid email first";
+            break;
+          default:
+            console.error(`unknown error ${error.code}, ${error.message}`);
+        }
+      });
+  }
+
+  signInWithGoogle() {
+    // Using a popup.
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope("profile");
+    provider.addScope("email");
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(result => {
+        this.$router.replace("/");
+      })
+      .catch(error => {
+        switch (error.code) {
+          case "auth/account-exists-with-different-credential":
+            firebase
+              .auth()
+              .fetchSignInMethodsForEmail(error.email)
+              .then(methods => {
+                const signInMethods = methods.join(",");
+                this.signinMethodText = `Your email ${error.email} is registered for the following sign-In methos: ${signInMethods}`;
+                this.signInMethodsDialog = true;
+              });
+            break;
+          case "auth/popup-blocked":
+          case "auth/popup-closed-by-user":
+            this.socialSignInProblemDialog = true;
+            break;
+          default:
+            console.error(`unknown error ${error.code}, ${error.message}`);
+        }
+      });
   }
 
   mounted() {
     (this.$refs["email"] as HTMLElement).focus();
+
+    // check if an signin from email is in progress
+    const url = new URL(window.location.href);
+    if (
+      firebase.auth().isSignInWithEmailLink(window.location.href) && // is signinlink
+      url.searchParams.has("email") // and contains email parameter
+    ) {
+      const email = url.searchParams.get("email");
+      if (email) {
+        firebase
+          .auth()
+          .signInWithEmailLink(email)
+          .then(() => {
+            this.$router.replace("/");
+          })
+          .catch(error => {
+            switch (error.code) {
+              case "auth/expired-action-code":
+                this.signInLinkExpiredDialog = true;
+                break;
+              case "auth/user-disabled":
+                this.accountLockedDialog = true;
+                break;
+              default:
+                console.error(`unkown error ${error.code}: ${error.message}`);
+            }
+          });
+      } else {
+        console.error("no email provided in link!");
+      }
+    }
   }
 }
 </script>
